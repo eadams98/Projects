b987cb67157a89754d85f285dc115e485ea9b738
Eric Adams <64985325+eadams98@users.noreply.github.com>
2021-09-24 17:07:43 -0500

    Fixed a few dynamic display issues (all data now matches up with correct column, next page button now only goes as far as needed, pages with no data entries don't cause an error). PROBLEMS TO TACKLE: fix graphical issue where first page doesn't match up with subsequent page layout (first page has a gap where as subequent pages elimate the gap. Has something to do with the 'STOP' logic

--- DB_Python_Workout_Tracker_APP/App.py
+++ DB_Python_Workout_Tracker_APP/App.py
@@ -17,6 +17,8 @@ class Application(tk.Frame):
         #self.canvas.grid(row=0)
         self.cache = {} # Dictionary to store pages generated
         self.page = "Start Page" # tells which page Currently on (Start Page, Tables Page, arm, etc.)
+        self.view = None # only for pages with multiple pages worth of information (Ex: tables view)
+        self.view_page_widget_placment_dictionary = None # maybe call it cache instead
 
         # Dedicated back button
         # only active if not on login  page (EITHER DISABLE OR PASS IF self.page = 'Start Page')
@@ -100,6 +102,12 @@ class Application(tk.Frame):
 
     def back_page(self):
         print('prevList: ', self.prev)
+
+        #if going back from a page with multiple turning pages reset it to self.view to None
+        if self.view != None:
+            self.view = None
+            self.view_page_widget_placment_dictionary = None
+
         # page to go back to
         prev = self.prev.pop((len(self.prev) - 1))
         print('prevList: ', self.prev)
@@ -125,6 +133,93 @@ class Application(tk.Frame):
         # self.page is Tables Page
         if self.page == "Tables Page":
             self.table_entries = None
+
+###########################################
+
+    def next_view(self, views, placement, nextBtn, backBtn): # views: list of list of tups, placment: dict w/ tup keys and list of tup values, currentView: int
+        self.view += 1
+        trueRow = 1
+
+        # if at last view disable next button
+        if self.view == len(views) - 1:
+            # disable next view button 
+            nextBtn["state"] = "disabled"
+
+        # if back button disabled enable it
+        if backBtn["state"] == "disabled":
+            backBtn["state"] = "normal"
+
+        print(self.view, nextBtn["state"], len(views), backBtn["state"])
+
+        # remove all old elements of old page, except for back, next view and back view buttons
+        for obj in self.master.grid_slaves():
+            if obj not in (self.back, nextBtn, backBtn):
+                obj.grid_remove()
+
+        # redundant code. clean up by making it into a function
+        if self.view in self.view_page_widget_placment_dictionary:
+
+            for obj in self.view_page_widget_placment_dictionary[self.view]:
+                obj.grid()
+        else:
+
+            #print("OGOOGAABOOGA:",  views, len(views[ self.view ][ views[self.view].index("STOP") + 1]) )
+            #print(data[self.view].index("STOP"))
+
+            '''
+            GAVE PROBLEMS (INDEX ERROR) IF DATA WAS EMPTY
+            if len(views[ self.view ][ views[self.view].index("STOP") + 1]) > 0:
+            '''
+            for idx in range(0, views[self.view].index("STOP")): # top columns
+                        print(views[self.view][idx])
+                        name = tk.Label(self.master, text=views[self.view][idx]) # all these will be tups with singular values
+                        place = placement[views[self.view][idx]]
+                        name.grid(column = place['column'], row = place['row'], padx = place['padx'], pady = place['pady'])
+
+            print('placement: ', placement)
+            for idx in range(views[self.view].index("STOP") + 1, len(views[self.view]) ): #row data (in tuple form)
+                
+                if views[self.view][idx] != "STOP": # CAUSE A LINE BREAK BETWEEN COLS AND ROWS
+                    for idx2, val in enumerate(views[self.view][idx]): # val = tuple(), idx2 = used for col indicator
+                        
+                        value = tk.Label(self.master, text=str(val))
+                        place = placement[views[self.view][idx2]]
+                        print('Val: ', val, 'place:', place) #place is correct up here but gets overwritten below
+                        
+                        if len(place) > 1:
+                            data = views[self.view]
+                            data = data[data.index("STOP") : idx]
+                            print("data: ",data, views[self.view][idx], placement[views[self.view][idx]], data.count(views[self.view][idx]))
+                            place = placement[ views[self.view][idx] ][ data.count(views[self.view][idx]) ]
+                            place = place[idx2] # !!!!!!!! FIX THIS: idk why returned item is contained/wrapped in another list
+                        print("place: ", place)
+                        value.grid(column = place['column'], row= place['row'], padx= place['padx'], pady= place['pady'])
+
+            # cache page regardless of number of columns
+            self.view_page_widget_placment_dictionary[self.view] = [obj for obj in self.master.grid_slaves() if obj not in (self.back, nextBtn, backBtn)]
+        
+
+    def back_view(self, views, placement, backBtn, nextBtn):
+        self.view -= 1
+        if self.view == 0: 
+            # disable back view button 
+            backBtn["state"] = "disabled"
+
+        if nextBtn["state"] == "disabled":
+            nextBtn["state"] = "normal"
+
+        # remove all old elements of old page, except for back, next view and back view buttons
+        for obj in self.master.grid_slaves():
+            if obj not in (self.back, nextBtn, backBtn):
+                obj.grid_remove()
+
+        if self.view in self.view_page_widget_placment_dictionary:
+            for obj in self.view_page_widget_placment_dictionary[self.view]:
+                obj.grid()
+
+        
+
+#############################################
         
     ### GUI altercation methods
     def switch_page(self, page=None):
@@ -207,7 +302,12 @@ class Application(tk.Frame):
             else: # incase of there being 4 or more rows make multiple views of said page with a max of 3 elems a page
                 
                 # list indicies indicate which page you're on (EX: views[0] = page 1's keys)
-                views = [ [] for x in range( ((len(keys) // 3) + 1) ) ] # make num of list needed for as many pages needed (each list contains keys for placement)
+
+                if len(keys) % 3 == 0:
+                    views = [ [] for x in range( ((len(keys) // 3)) ) ] # make num of list needed for as many pages needed (each list contains keys for placement)
+                else:
+                    views = [ [] for x in range( ((len(keys) // 3) + 1) ) ] # make num of list needed for as many pages needed (each list contains keys for placement)
+                
                 placement = {} # create dictionary (key: name of col, value: nums responsible for placement in grid)
                 
                 for col, key in enumerate(keys):
@@ -217,8 +317,14 @@ class Application(tk.Frame):
 
                     # these are singular string names. I keep them as tuples just to match the rest of the entries in
                     # placement dictionary
+                    print(col,len(views))
                     views[col//3].append(tuple([key]))
-                    placement[tuple([key])] = {"column" : col, "row" : 0,  "padx" : 10, "pady" : 10}
+                    placement[tuple([key])] = {"column" : col % 3, "row" : 0,  "padx" : 10, "pady" : 10}
+
+                # Figure this is a sloppy solution, but its responsible to indicationg where the seperation between 
+                # column names and actual data entries (rows)
+                for key_list in views:
+                    key_list.append("STOP")
 
                 # row 0 is reserved for names of table
                 trueRow = 1 
@@ -263,18 +369,18 @@ class Application(tk.Frame):
                                 if tuple(tup_key) not in placement:
                                     placement[tuple(tup_key)] = [
                                                                 [
-                                        {"column" : idx - 2, "row" : trueRow + row, "padx" : 2, "pady" : 2},
-                                        {"column" : idx - 1, "row" : trueRow + row, "padx" : 2, "pady" : 2},
-                                        {"column" : idx, "row" : trueRow + row, "padx" : 2, "pady" : 2}
+                                        {"column" : (idx - 2) % 3, "row" : trueRow + row, "padx" : 2, "pady" : 2},
+                                        {"column" : (idx - 1) % 3, "row" : trueRow + row, "padx" : 2, "pady" : 2},
+                                        {"column" : idx % 3, "row" : trueRow + row, "padx" : 2, "pady" : 2}
                                                                 ]                            
                                                                 ]
 
                                 else:
                                     placement[tuple(tup_key)].append(
                                         [
-                                        {"column" : idx - 2, "row" : trueRow + row, "padx" : 2, "pady" : 2},
-                                        {"column" : idx - 1, "row" : trueRow + row, "padx" : 2, "pady" : 2},
-                                        {"column" : idx, "row" : trueRow + row, "padx" : 2, "pady" : 2}
+                                        {"column" : (idx - 2) % 3, "row" : trueRow + row, "padx" : 2, "pady" : 2},
+                                        {"column" : (idx - 1) % 3, "row" : trueRow + row, "padx" : 2, "pady" : 2},
+                                        {"column" : idx % 3, "row" : trueRow + row, "padx" : 2, "pady" : 2}
                                         ] 
                                         )
 
@@ -286,34 +392,54 @@ class Application(tk.Frame):
                                 views[idx//3].append( tuple(tup_key) )
 
                                 for x in range(len(tup_key)):
-                                    tmp[0].append({"column" : idx - (x-2), "row" : trueRow + row, "padx" : 2, "pady" : 2})
+                                    
+                                    tmp[0].append({"column" : (x) % 3, "row" : trueRow + row, "padx" : 2, "pady" : 2})
 
                                 if tuple(tup_key) not in placement:
                                     placement[tuple(tup_key)] = tmp
                                 else:
-                                    placement[tuple(tup_key)].append(tmp)
+                                    placement[tuple(tup_key)].append(tmp[0])
                                 
                             
                             else:
                                 #views[idx//3].append( tuple(tup_key) )
-                                tup_key.append(dictionary[keys[idx]])
+                                tup_key.append( dictionary[keys[idx]] )
                 print(placement.items())
                 print(len(placement.keys()), len(views))
                 print()
-                print("views: ", views)
+                print("views: ", views, len(views))
+                self.view = 0 # ONLY SHOULD HAVE A VALUE IF MULTIPLE PAGES TO TURN TO EXIST
+
+                if len(views) > 1:
+                    #tk.Button(self.master, text="View", command= lambda t=tableName, selected = self.list_box: self.open_table_view(t, selected))
+                    #self.button.grid(column=idx, row= 2)
+                    next_view = tk.Button(self.master, text = "next view", command= lambda: self.next_view(views, placement, next_view, back_view))
+                    next_view.grid(column = 3, row = 1)
+                    back_view = tk.Button(self.master, text = "back view", command= lambda: self.back_view(views, placement, back_view, next_view))
+                    back_view.grid(column = 3, row = 2)
+                    back_view["state"] = "disabled"
+
                 for idx in range(3): # top columns
                     print(views[0][idx])
                     name = tk.Label(self.master, text=views[0][idx]) # all these will be tups with singular values
                     place = placement[views[0][idx]]
                     name.grid(column = place['column'], row = place['row'], padx = place['padx'], pady = place['pady'])
 
-                for idx in range(3, len(views[0]) + 1 ): #row data (in tuple form)
-                    print(views[0][idx])
-                    #value = tk.Label(self.master, text=dictionary[keys[idx]])
-                            #value.grid(column = idx, row= trueRow + row, padx= 2, pady= 2)
-                    for idx2, val in enumerate(views[0][idx]): # val = tuple()
-                        value = tk.Label(self.master, text=str(val))
-                        value.grid(column = idx2, row= trueRow + (idx-3), padx= 2, pady= 2)
+                for idx in range(3, len(views[0]) ): #row data (in tuple form)
+                    
+                    
+                    if views[0][idx] != "STOP": # CAUSE A LINE BREAK BETWEEN COLS AND ROWS
+                        for idx2, val in enumerate(views[0][idx]): # val = tuple()
+                            value = tk.Label(self.master, text=str(val))
+                            value.grid(column = idx2, row= trueRow + (idx-3), padx= 2, pady= 2)
+
+                self.view_page_widget_placment_dictionary = {}
+                self.view_page_widget_placment_dictionary[self.view] = [obj for obj in self.master.grid_slaves() if obj not in (self.back, next_view, back_view)]
+                '''
+                for obj in self.master.grid_slaves():
+                    if obj not in (self.back, nextBtn, backBtn):
+                        obj.grid_remove()
+                '''
 
 
 
